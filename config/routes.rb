0acc7dc0abe7a551e3c4228e202fef820c2c1cd3
lindagd <liwdag@gmail.com>
2024-09-30 18:37:52 -0300

    :sparkles: feat(index): cria controller Home, root_path, e página inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  
   resources :genres, only: [:edit, :update, :show, :create, :new]
   resources :directors, only: [:edit, :update, :show, :create, :new]
   resources :films, only: [:edit, :update, :show, :create, :new]
