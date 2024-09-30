411a3dcf7b62a7bd4b18ebe693fad459f2d8c4fb
lindagd <liwdag@gmail.com>
2024-09-30 17:23:25 -0300

    :sparkles: feat(director): cria model Director

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_200727) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_202046) do
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.string "nationality"
+    t.date "birth_date"
+    t.integer "favorite_genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["favorite_genre_id"], name: "index_directors_on_favorite_genre_id"
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "directors", "favorite_genres"
 end
