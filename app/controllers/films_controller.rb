da338d866c15c655c377fd425e5583d35d9c0910
lindagd <liwdag@gmail.com>
2024-10-01 09:35:07 -0300

    :sparkles: feat(film): Adiciona Create, Read e Update do model Film

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,47 @@
+class FilmsController < ApplicationController
+  def show
+    @film = Film.find params[:id]
+  end
+
+  def new
+    @film = Film.new
+  end
+
+  def create 
+    @film = Film.new(title: params[:film][:title],
+                release_year: params[:film][:release_year],
+                synopsis: params[:film][:synopsis],
+                country_of_origin: params[:film][:country_of_origin],
+                running_time: params[:film][:running_time],
+                director_id: params[:film][:director_id],
+                genre_id: params[:film][:genre_id] 
+              )
+
+    if @film.save
+      return redirect_to film_path(@film.id)
+    end
+
+    render :new
+  end
+
+  def edit 
+    @film = Film.find params[:id]
+  end
+
+  def update
+    @film = Film.find params[:id]
+    
+    if @film.update(title: params[:film][:title],
+              release_year: params[:film][:release_year],
+              synopsis: params[:film][:synopsis],
+              country_of_origin: params[:film][:country_of_origin],
+              running_time: params[:film][:running_time],
+              director_id: params[:film][:director_id],
+              genre_id: params[:film][:genre_id] 
+            )
+      return redirect_to film_path(@film.id)
+    end
+
+    render :edit
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @films = Film.all
   end
 end
